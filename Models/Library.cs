77fe03dcfb5bb721df1a771387372d3483ad3369
Brett Wilcox <wilcox.brett@gmail.com>
2019-03-28 16:30:52 -0600

    Simple console library project.

---
+++ Models/Library.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+
+namespace console_library.Models
+{
+
+  public class Library
+  {
+    public string Location { get; set; }
+    public string Name { get; set; }
+    public List<Book> Books { get; private set; }
+    public List<Book> CheckedOut { get; private set; }
+
+    public void PrintBooks(List<Book> books)
+    {
+      for (int i = 0; i < books.Count; i++)
+      {
+        Console.WriteLine(value: $"{i + 1}. {books[i].Title} by {books[i].Author}");
+      }
+    }
+
+    public void AddBook(Book book)
+    {
+      Books.Add(book);
+    }
+
+    public void Checkout(string selection)
+    {
+      Book selectedBook = ValidateBook(selection, Books);
+
+      if (selectedBook == null)
+      {
+        Console.Clear();
+        System.Console.WriteLine(@"Invalid Selection
+                        ");
+        return;
+      }
+      else
+      {
+        selectedBook.Available = false;
+        CheckedOut.Add(selectedBook);
+        Books.Remove(selectedBook);
+        Console.WriteLine($"Congrats, you checked out {selectedBook.Title} by {selectedBook.Author}.");
+        Console.WriteLine(@"
+Here are the remaining books in the library:");
+      }
+    }
+
+    public void CheckIn(string selection)
+    {
+      Book selectedBook = ValidateBook(selection, CheckedOut);
+      if (selectedBook == null)
+      {
+        Console.Clear();
+        System.Console.WriteLine(@"Invalid Selection
+                        ");
+        return;
+      }
+      else
+      {
+        selectedBook.Available = true;
+        Books.Add(selectedBook);
+        CheckedOut.Remove(selectedBook);
+        Console.Clear();
+        Console.WriteLine($"Congrats, you returned {selectedBook.Title} by {selectedBook.Author}.");
+        Console.WriteLine(@"
+Available books:");
+      }
+    }
+
+    private Book ValidateBook(string selection, List<Book> bookList)
+    {
+      int bookIndex;
+      bool valid = Int32.TryParse(selection, out bookIndex);
+      if (!valid || bookIndex < 0 || bookIndex > bookList.Count)
+      {
+        return null;
+      };
+      return bookList[bookIndex - 1];
+    }
+
+
+    public Library(string location, string name)
+    {
+      Location = location;
+      Name = name;
+      Books = new List<Book>()
+      {
+      new Book("Where the Sidewalk Ends", "Shel Silverstein"),
+      new Book("Things Fall Apart", "Chinua Achebe"),
+      new Book("A New Earth", "Eckhart Tolle"),
+      new Book("Ficciones", "Jorge Luis Borges"),
+    };
+      CheckedOut = new List<Book>();
+
+    }
+
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using console_library.Models;
+
+namespace console_library
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool inLibrary = true;
+      Library myLibrary = new Library("Boise", "Capital Library");
+      Console.Clear();
+      Console.WriteLine(@"Welcome to Capital Library.
+      
+Available Books:");
+      while (inLibrary)
+      {
+        myLibrary.PrintBooks(myLibrary.Books);
+        Console.WriteLine(@"
+Select a number to checkout book. Press Q to quit, or R to return a book.");
+        string selection = Console.ReadLine().ToLower();
+        Console.Clear();
+        if (selection == "q")
+        {
+          Console.WriteLine("Sorry to see you go. Goodbye.");
+          return;
+        }
+        else if (selection == "r")
+        {
+          Console.WriteLine(@"Here are the books you have checked out:");
+          myLibrary.PrintBooks(myLibrary.CheckedOut);
+          Console.WriteLine(@"
+Select a book to return.");
+          string bookToReturn = Console.ReadLine().ToLower();
+          myLibrary.CheckIn(bookToReturn);
+        }
+        else
+        {
+          myLibrary.Checkout(selection);
+        }
+
+      }
+    }
+  }
+}
